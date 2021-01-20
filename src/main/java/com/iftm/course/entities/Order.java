95069336a6944a5016696e258aebe5f025150dbe
Kelvin Arruda <kelvin.k.as@hotmail.com>
2021-01-20 16:40:46 -0300

    Subtotal & Total methods

--- src/main/java/com/iftm/course/entities/Order.java
+++ src/main/java/com/iftm/course/entities/Order.java
@@ -103,7 +103,13 @@ public class Order implements Serializable {
 		}
 	}
 	
-	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getsubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
